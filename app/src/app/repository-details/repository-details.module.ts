46882f730cb053c6a1e6a70ed571839f8dd5e348
Thomas Sablik <thomas.sablik@wombytes.de>
2017-04-17 02:54:03 +0200

    Implement repository details

--- app/src/app/repository-details/repository-details.module.ts
+++ app/src/app/repository-details/repository-details.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { RepositoryDetailsComponent } from './components/repository-details.component';
+import { RepositoryDetailsService } from './services/repository-details.service';
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpModule
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     RepositoryDetailsComponent
   ],
   providers: [
+    RepositoryDetailsService
   ],
   exports: [
     RepositoryDetailsComponent
