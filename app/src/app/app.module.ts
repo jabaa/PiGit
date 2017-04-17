0cbd72553e7b39dbcaf8b18a7dcb669c4151683f
Thomas Sablik <thomas.sablik@wombytes.de>
2017-04-17 02:43:17 +0200

    Add rxjs operators to AppModule

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { appRoutes } from './app.routes';
 import { RepositoryListModule } from './repository-list/repository-list.module';
 import { RepositoryDetailsModule } from './repository-details/repository-details.module';
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
 @NgModule({
   imports: [
     BrowserModule,
